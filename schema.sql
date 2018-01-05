ba9612f60e06a0ae4abebc1d0a84f2acc0f1591b
orlandoecool <orlando.torres12@upr.edu>
2018-01-05 12:46:54 -0400

    Completed schema.sql

---
+++ schema.sql
@@ -0,0 +1,103 @@
+-- This file contains the definitions of the tables used in the application.
+--
+-- Users table
+create table users(uID serial primary key, uFirstName varchar(20), uLastName varchar(20), uGender char(1), uBirthDate date);
+
+-- Addresses table
+create table addresses(addID serial primary key, uID integer references users(uID), city varchar(20), street varchar(30), country varchar(20), zipCode integer);
+
+-- Credentials table
+create table credentials(cid serial primary key, uID integer references users(uID), username varchar(20), password varchar(20), email varchar(20));
+
+-- Credit Card table
+create table creditCards(ccID serial primary key, uID integer references users(uID), ccNumber integer, ccExpDate date, ccSecurityCode integer);
+
+-- Telephone Numbers table
+create table telephoneNumbers(tID serial primary key, uID integer references users(uID), homeNumber integer, mobileNumber integer, workNumber integer, otherNumber integer);
+
+-- Admins table
+create table admins(adminID serial primary key, uID integer references users(uID));
+
+-- Suppliers table
+create table suppliers(suppID serial primary key, uID integer references users(uID));
+
+-- Requesters table
+create table requesters(reqID serial primary key, uID integer references users(uID));
+
+-- Market table
+create table market(mID serial primary key, suppID integer references suppliers(suppID), mDate date, mQty integer, mReserved integer, mAvaiable integer, invPrice float);
+
+-- Price History table
+create table priceHistory(phID serial primary key, mID integer references market(suppID), startDate date, thruDate date, priceAtMoment float);
+
+-- Purchases table
+create table purchases(reqID integer references requesters(reqID), mID integer references market(mID), purchaseDate date, purchaseAmount float, primary key (reqID, mID));
+
+-- Reserves table
+create table reserves(reqID integer references requesters(reqID), mID integer references market(mID), resQty integer, resDate date, resExpDate date, primary key (reqID, mID));
+
+-- Resources table
+create table resources(resID serial primary key, resCategory varchar(20));
+
+-- Requests table
+create table requests(reqID integer references requesters(reqID), resID integer references resources(resID), requestQty integer, requestDate date, primary key(reqID, resID));
+
+-- Sells table
+create table sells(mID integer references market(mID), resID integer references resources(resID), primary key (mID, resID));
+
+-- Food table
+create table food(fID serial primary key, resID integer references resources(resID), fCategory varchar(20), fExpDate date);
+
+-- Baby Food table
+create table babyFood(bfID serial primary key, fID integer references food(fID), bfName varchar(20), bfBrand varchar(20), bfOz float);
+
+-- Canned Food table
+create table cannedFood(cfID serial primary key, fID integer references food(fID), cfName varchar(20), cfBrand varchar(20), cfOz float);
+
+-- Dry Food table
+create table dryFood(dfID serial primary key, fID integer references food(fID), dfName varchar(20), dfBrand varchar(20), dfOz float);
+
+-- Medications table
+create table medications(medID serial primary key, resID integer references resources(resID), medName varchar(20), medBrand varchar(20), medType varchar(20), medOz float, medExpDate date);
+
+-- Ice table
+create table ice(iceID serial primary key, resID integer references resources(resID), iceWeight float, iceContainer varchar(20));
+
+-- Tools table
+create table tools(tID serial primary key, resID integer references resources(resID), tName varchar(20), tType varchar(20), tWeight float, tSize float);
+
+-- Clothing table
+create table clothing(cID serial primary key, resID integer references resources(resID), clothGenger char(1), tType varchar(20), clothBrand varchar(20), clothSize varchar(10), clothColor varchar(20), clothDesignPatterns varchar(20));
+
+-- Water table
+create table water(wID serial primary key, resID integer references resources(resID), wCategory varchar(20), wExpDate date);
+
+-- Water Bottles table
+create table waterBottles(wbID serial primary key, wID integer references water(wID), wbBrand varchar(20), wbOz float, wbMaterial varchar(20));
+
+-- Water Gallons table
+create table waterGallons(wgID serial primary key, wID integer references water(wID), wgBrand varchar(20), wgMaterial varchar(20));
+
+-- Power Generators table
+create table powerGenerators(pgID serial primary key, resID integer references resources(resID), pgBrand varchar(20), pgType varchar(20), pgPower integer, pgFuelType varchar(10));
+
+-- Batteries table
+create table batteries(bID serial primary key, resID integer references resources(resID), bType varchar(20), bBrand varchar(20));
+
+-- Medical Devices table
+create table medicalDevices(mdID serial primary key, resID integer references resources(resID), mdName varchar(20), mdType varchar(20), mdWeight float, mdSize float);
+
+-- Heavy Equipment table
+create table heavyEquipment(heID serial primary key, resID integer references resources(resID), heName varchar(20), heType varchar(20), heWeight float, heSize float);
+
+-- Fuel table
+create table fuel(fuelID serial primary key, resID integer references resources(resID), fuelCategory varchar(10));
+
+-- Diesel table
+create table diesel(dID serial primary key, fuelID integer references fuel(fuelID), dLiters float, dContainer varchar(20));
+
+-- Propane table
+create table propane(pID serial primary key, fuelID integer references fuel(fuelID), pLiters float, pContainer varchar(20));
+
+-- Gasoline Food table
+create table gasoline(dID serial primary key, fuelID integer references fuel(fuelID), gLiters float, gContainer varchar(20));
