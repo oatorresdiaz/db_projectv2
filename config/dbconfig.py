f0121c2e5a5646f50b0b6bc0ca567ef8c2bbf315
orlandoecool <orlando.torres12@upr.edu>
2018-01-06 16:19:16 -0400

    Setup dbconfig for heroku database access.

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,7 +1,7 @@
 #Database configuration information
 
 pg_config = {
-    'user' : 'postgres',
-    'passwd' : 'Schultz123',
-    'dbname' : 'db_project'
+    'user' : 'aobrivoabafzlz',
+    'passwd' : '434864b4bc16a8ad9c257c2265f8ec9c093d4aaed67ec3ee10abade0e6bab97a',
+    'dbname' : 'd74ce819nv1kad'
 }
