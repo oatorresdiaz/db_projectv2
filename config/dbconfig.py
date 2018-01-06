0b22c758d6077c4941c7b6c6efe991f713b23f23
orlandoecool <orlando.torres12@upr.edu>
2018-01-06 17:11:08 -0400

    Setup dbconfig for heroku database access.

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -3,5 +3,7 @@
 pg_config = {
     'user' : 'aobrivoabafzlz',
     'passwd' : '434864b4bc16a8ad9c257c2265f8ec9c093d4aaed67ec3ee10abade0e6bab97a',
-    'dbname' : 'd74ce819nv1kad'
+    'dbname' : 'd74ce819nv1kad',
+    'host': 'ec2-54-225-255-132.compute-1.amazonaws.com',
+    'port': '5432'
 }
