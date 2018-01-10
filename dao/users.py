886b486ecac03bc2ba0eaf7673dfd56b2de0f069
orlandoecool <orlando.torres12@upr.edu>
2018-01-09 20:34:23 -0400

    Created functions in user handler and DAO for searching users with arguments and sorting.

--- dao/users.py
+++ dao/users.py
@@ -55,11 +55,39 @@ class UsersDAO:
         values = list(args.values())
         for arg in args:
             arguments = arguments + arg + "= %s" + " and "
-        arguments = arguments[:-5]  + ";" #Remove the last ' and '
+        arguments = arguments[:-5]  # Remove the last ' and '
         query = "select * from users natural inner join addresses natural inner join telephonenumbers where " + arguments
         cursor.execute(query, values)
         result = []
         for row in cursor:
             result.append(row)
         return result
+
+    def searchUsersWithSorting(self, orderby):
+        cursor = self.conn.cursor()
+        query = "select * from users natural inner join addresses natural inner join telephonenumbers order by " + orderby
+        cursor.execute(query)
+        print(cursor.query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def searchUsersByArgumentsWithSorting(self, args):
+        cursor = self.conn.cursor()
+        arguments = ""
+        values = list(args.values())
+        values.remove(args.get('orderby'))
+        for arg in args:
+            if arg != 'orderby':
+                arguments = arguments + arg + "= %s" + " and "
+        arguments = arguments[:-5]  # Remove the last ' and '
+        query = "select * from users natural inner join addresses natural inner join telephonenumbers where " + arguments + " order by " + args.get('orderby')
+        cursor.execute(query, values)
+        print(cursor.query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
 
